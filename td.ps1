f36c7663cecf013f1dc3b4865ffaf92072daf550
Desi Quintans <science@desiquintans.com>
2024-07-02 12:34:33 +1000

    Update td.ps1
    Changes:
    
    1. Move away from organising tasks into subfolders and towards a flat structure, with tasks named as "PROJECT - Task", where PROJECT is used as a sorting layer.
    
    2. Show task list as nice table instead of hard-to-style tree.
    
    3. Do not overwrite existing files (and clean extensions and `./` from inputs to reduce the risk of duplicate files).

--- td.ps1
+++ td.ps1
@@ -1,54 +1,90 @@
-﻿# td.ps1 (todo)
+# td.ps1 (todo)
 # Inspired by: https://github.com/holman/dotfiles/commit/d774e970a88a04aca8024178849301af6d6ac5c3
-# Creates .md text files so that I can put task-related info inside them if necessary.
+#
+# A to-do system that uses loose Markdown files to track tasks. Each file is an action item, and
+# can contain notes relevant to that task. You should name your tasks as
+#    PROJECTNAME - Task description
+# so that they show up nicely in the task list table.
 #
 # Usage:
-#     > td my-proj/prepare first draft
+#     > td CHAPTER 1 - Prepare first draft
 #
-#     Created:
-#       Desktop\my-proj\prepare first draft.md
+#         Created         Project         Task
+#         -------         -------         ----
+#         2024-06-24      CHAPTER 1       Prepare first draft
 #
-#     +---my-proj
-#     |       download newest data
-#     |    -> prepare first draft
-#     |       tree measurement field trip 2023-05-07
-#     |
-#     \---personal
-#             mow lawn
-#             pick up moon cakes
+#         The task's file is located at:
+#         C:\Users\xxx\Desktop\CHAPTER 1 - Prepare first draft.md
+#
+#     > td
+#
+#         Created         Project         Task
+#         -------         -------         ----
+#         2024-06-24      CHAPTER 1       Prepare first draft
+#
 
 
 
-# Desktop isn't always in ~\Desktop\, so it has to be found this way.
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
+# Establish where the files will be created. If you want to put them on your desktop, use:
+$ToDoPath = [Environment]::GetFolderPath("Desktop")
+# $ToDoPath = "C:\Users\myusername\path\to\folder"
 
-$TaskPath = $args -replace "[$([RegEx]::Escape([string][IO.Path]::GetInvalidPathChars()))]+", " "
+
+
+# This function prints a table of current projects and tasks, sorted first by project and then by creation date.
+function Show-Tasks {
+    # This is what the table will look like.
+    $tdlayout = @{Label = 'Created'; Expression = {$_.CreationTime -replace '^(.{10}).*?$', '$1'}; Width = 15},
+                @{Label = 'Project'; Expression = {$_.Name -replace '^(.*?) - .*?$', '$1'};        Width = 15},
+                @{Label = 'Task';    Expression = {$_.Name -replace '^.*? - (.*?)\.md$', '$1'}}
+
+    Get-ChildItem -Filter *.md |
+    Sort-Object Name, CreationTime |
+    Format-Table -Property $tdlayout -Wrap |
+    Out-String |
+    Write-Host
+}
+
+
+# If no arguments are provided, just print the task list.
+if($args.Count -eq 0) {
+    Show-Tasks
+    return
+}
+
+
+
+# If arguments are provided, treat them as the name of a new task. Clean the
+# input so that it can be used as a valid filename.
+$TaskPath = $args -replace '[:*?\"<>|]+', "_"
 $TaskPath = $TaskPath -replace "/", "\"
+$TaskPath = $TaskPath -replace "(\.md)+$", ""
+$TaskPath = $TaskPath -replace "^(.\\)+", ""
 $TaskName = Split-Path -Path $TaskPath -Leaf
-$FullPath = "$DesktopPath\$TaskPath.md"
+$FullPath = "$ToDoPath\$TaskPath.md"
 
 
-# Parts of the new file.
+# Contents of the file that will be created.
 $DateStamp = Get-Date -Format yyyy-MM-dd
 $Contents  = @"
 # $TaskPath ($DateStamp)
 
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 
 "@
 
-New-Item "$FullPath" -Force -Value $Contents | Out-Null
 
-# Saving $mytree ... | Out-String and then Write-Hosting it makes it behave
-# nicely when td is used in a pipeline.
-$mytree = 
-    & tree $DesktopPath /F /a |
-        Select-String -Pattern '(^.-+|^.\s+$|\.md$)' | 
-        % {$_ -replace "\.md$",""} |
-        % {$_ -replace "(^.\s+)(\s{3})($($TaskName)$)", "`$1-> `$3"} |
-        Out-String
 
-Write-Host $mytree
+# Create the file if it doesn't already exist.
+if (-not (Test-Path -Path $FullPath)) {
+    New-Item -Path "$FullPath" -Force -Value $Contents | Out-Null
+}
 
+# Print the updated task list.
+Show-Tasks
+
+# Print a little message about the task file.
+# Returning the full path to the new file allows me to pipe into `td my_new_task | ii`.
+Write-Host "The task's file is located at:" -ForegroundColor green
 return $FullPath
